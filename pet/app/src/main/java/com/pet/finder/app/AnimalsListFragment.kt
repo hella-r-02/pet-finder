9dc1a933aa879b928d98adfb3944397d3540d30d
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-08-06 13:45:19 +0300

    Add animals list

---
+++ pet/app/src/main/java/com/pet/finder/app/AnimalsListFragment.kt
@@ -0,0 +1,21 @@
+package com.pet.finder.app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AnimalsListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_animals_list, container, false)
+    }
+}
